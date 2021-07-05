fccab3fe5bd6429f7e6483a72548fc9e046ddc9b
giovanni-mormone <giovanni.mormone@studio.unibo.it>
2021-07-05 15:33:27 +0200

    added methods to create a problem, load the problem data(basic), solve it and retrieve some result; added struct to manage problem pointer

--- WrapperCoinMP/WrapperCoin.cs
+++ WrapperCoinMP/WrapperCoin.cs
@@ -4,26 +4,73 @@ using System.Text;
 using WrapperCoinMP;
 namespace WrapperCoinMP
 {
-    public partial class WrapperCoin
+
+    public struct WrapProblem
     {
-        private int resultInit;
-
-        public double GetVersion() => CoinGetVersion();
-        public string GetVersionStr() => Marshal.PtrToStringAnsi(CoinGetVersionStrIntPtr());
-
-
-        public WrapperCoin()
+        private IntPtr problem;
+        
+        public WrapProblem(IntPtr problem)
         {
-            resultInit = CoinInitSolver("");
-            if(resultInit != 0)
-            {
-
-            }
+            this.problem = problem;
         }
-        public string GetSolverName()
+        public IntPtr getProblem()
+        {
+            return problem;
+        }
+
+    }
+    public static partial class WrapperCoin
+    {
+        private const int SOLV_CALL_SUCCESS = 0;
+        private const int SOLV_CALL_FAILED = -1;
+
+        public const int SOLV_OBJSENS_MAX = -1;
+        public const int SOLV_OBJSENS_MIN = 1;
+
+        public const int SOLV_METHOD_DEFAULT = 0;
+
+        public static double GetVersion() => CoinGetVersion();
+        public static string GetVersionStr() => Marshal.PtrToStringAnsi(CoinGetVersionStrIntPtr());
+        public static int FreeSolver() => CoinFreeSolver();
+        public static string GetSolverName()
         {
             StringBuilder builder = new StringBuilder();
             CoinGetSolverNameBuf(builder, builder.Capacity);
+            return builder.ToString();
+        }
+        public static double GetInfinity() => CoinGetInfinity();
+        public static WrapProblem CreateProblem(string problemName) => new WrapProblem(CoinCreateProblem(problemName));
+
+        public static int LoadProblem(WrapProblem problem, int colCount, int rowCount, int nzCount,
+                        int rangeCount, int objectSense, double objectConst, double[] objectCoeffs,
+                        double[] lowerBounds, double[] upperBounds, char[] rowType, double[] rhsValues,
+                        double[] rangeValues, int[] matrixBegin, int[] matrixCount, int[] matrixIndex,
+                        double[] matrixValues, string[] colNames, string[] rowNames, string objName) =>
+                            CoinLoadProblemBuf(problem.getProblem(), colCount, rowCount, nzCount, rangeCount, objectSense, objectConst,
+                                objectCoeffs, lowerBounds, upperBounds, rowType, rhsValues, rangeValues, matrixBegin, matrixCount, matrixIndex,
+                                matrixValues, BufferizeArray(colNames), BufferizeArray(rowNames), objName);
+
+        public static int OptimizeProblem(WrapProblem problem)
+        {
+            return CoinOptimizeProblem(problem.getProblem(), SOLV_METHOD_DEFAULT);
+        }
+
+        public static int GetSolutionValues(WrapProblem problem, [In, Out] double[] activity,
+                       [In, Out] double[] reducedCost, [In, Out] double[] slackValues, [In, Out] double[] shadowPrice) =>
+            CoinGetSolutionValues(problem.getProblem(), activity, reducedCost, slackValues, shadowPrice);
+
+        private static string BufferizeArray(string[] toBuff)
+        {
+            StringBuilder builder = new();
+            if(toBuff.GetLength(0) > 0)
+            {
+                int position = 0;
+                while(position < toBuff.GetLength(0))
+                {
+                    builder.Append(toBuff[position] + "\0");
+                    position++;
+                }
+            }
             return builder.ToString();
         }
     }
--- WrapperCoinMP/WrapperCoinMPImports.cs
+++ WrapperCoinMP/WrapperCoinMPImports.cs
@@ -7,13 +7,26 @@ using System.Threading.Tasks;
 
 namespace WrapperCoinMP
 {
-    public partial class WrapperCoin
+    public static partial class WrapperCoin
     {
         [DllImport("Coinmp.dll")] static extern int CoinInitSolver(string licenseStr);
         [DllImport("Coinmp.dll")] static extern int CoinFreeSolver();
 
-        [DllImport("coinmp.dll")] public static extern int CoinGetSolverNameBuf(StringBuilder solverName, int buflen);
-        [DllImport("Coinmp.dll")] public static extern double CoinGetVersion();
-        [DllImport("coinmp.dll", EntryPoint = "CoinGetVersionStr")] public static extern IntPtr CoinGetVersionStrIntPtr();
+        [DllImport("coinmp.dll")] static extern int CoinGetSolverNameBuf(StringBuilder solverName, int buflen);
+        [DllImport("Coinmp.dll")] static extern double CoinGetVersion();
+        [DllImport("Coinmp.dll", EntryPoint = "CoinGetVersionStr")] static extern IntPtr CoinGetVersionStrIntPtr();
+        [DllImport("Coinmp.dll")] static extern double CoinGetInfinity();
+        [DllImport("Coinmp.dll")] static extern IntPtr CoinCreateProblem(string problemName);
+        [DllImport("Coinmp.dll")]
+        public static extern int CoinLoadProblemBuf(IntPtr hProb, int colCount, int rowCount,
+                        int nzCount, int rangeCount, int objectSense, double objectConst, double[] objectCoeffs,
+                        double[] lowerBounds, double[] upperBounds, char[] rowType, double[] rhsValues,
+                        double[] rangeValues, int[] matrixBegin, int[] matrixCount, int[] matrixIndex,
+                        double[] matrixValues, string colNamesBuf, string rowNamesBuf, string objName);
+        [DllImport("Coinmp.dll")] static extern int CoinOptimizeProblem(IntPtr hProb, int method);
+
+        [DllImport("Coinmp.dll")]
+        static extern int CoinGetSolutionValues(IntPtr hProb, [In, Out] double[] activity,
+                       [In, Out] double[] reducedCost, [In, Out] double[] slackValues, [In, Out] double[] shadowPrice);
     }
 }
